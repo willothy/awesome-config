890dfcd72b6d6bc46d48c393a23e55a9581f7131
Will Hopkins <willothyh@gmail.com>
2023-08-30 22:52:48 -0700

    updates

--- autostart.lua
+++ autostart.lua
@@ -8,8 +8,7 @@ local conf = gfs.get_configuration_dir()
 local screens = conf .. "screens.sh"
 awful.spawn.with_shell(screens)
 
-local picom_cfg = conf .. "assets/picom/picom.conf"
-awful.spawn("picom --config " .. picom_cfg .. " -b")
+awful.spawn("picom")
 
 awful.spawn("nm-applet")
 awful.spawn.with_shell("lxpolkit")
