3da91a3fca8862c377fcd45ebefd48cab1e58810
Will Hopkins <willothyh@gmail.com>
2023-11-29 02:05:09 -0800

    compositor: replace picom with compfy

--- autostart.lua
+++ autostart.lua
@@ -8,7 +8,8 @@ local conf = gfs.get_configuration_dir()
 local screens = conf .. "screens.sh"
 awful.spawn.with_shell(screens)
 
-awful.spawn("picom")
+-- awful.spawn("picom")
+awful.spawn("compfy")
 
 awful.spawn("nm-applet")
 awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
