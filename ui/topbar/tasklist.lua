8ec435d2222930e1810d70d8f1fba3d9a47fe79b
Will Hopkins <willothyh@gmail.com>
2024-01-30 18:33:11 -0800

    small sidecar Rust module, better task preview
    reimplemented task preview myself based on Bling's implementation
    because theirs had some bugs. Also fixes some icon things.

--- ui/topbar/tasklist.lua
+++ ui/topbar/tasklist.lua
@@ -1,47 +1,25 @@
-local M = {}
+---@class TaskList
+local TaskList = {}
 
 local awful = Capi.awful
-local gears = Capi.gears
 local wibox = Capi.wibox
 local beautiful = Capi.beautiful
 
-require("vendor.bling").widget.task_preview.enable({
-  placement_fn = function(c)
-    awful.placement.top(c, {
-      margins = {
-        top = beautiful.bar_height + beautiful.useless_gap * 2,
-      },
-    })
-  end,
-  width = 300,
-  height = 300,
-})
-
 client.connect_signal("request::manage", function(c)
-  local icon = beautiful.icon_theme:get_client_icon_path(c)
-  local surface = Capi.gears.surface(icon)
-  c.icon = surface._native
+  beautiful.icon_theme:get_client_icon_path(c, function(icon)
+    local surface = Capi.gears.surface(icon)
+    c.icon = surface._native
+  end)
 end)
 
-function M.new(s)
+function TaskList.new(screen)
+  local preview = require("ui.task_preview").new(screen)
+
   return awful.widget.tasklist({
-    screen = s,
+    screen = screen,
     filter = awful.widget.tasklist.filter.currenttags,
     source = function()
-      local ret = {}
-
-      local found = {}
-
-      for _, t in ipairs(s.tags) do
-        for _, c in ipairs(t:clients()) do
-          if not found[c] then
-            table.insert(ret, c)
-          end
-          found[c] = true
-        end
-      end
-
-      return ret
+      return screen.all_clients
     end,
     buttons = {
       awful.button({}, 1, function(c)
@@ -61,49 +39,35 @@ function M.new(s)
         awful.client.focus.byidx(1)
       end),
     },
-    style = {
-      shape = function(cr, w, h)
-        return gears.shape.rounded_rect(cr, w, h, 5)
-      end,
-    },
     layout = {
-      spacing = 5,
+      spacing = 10,
       layout = wibox.layout.fixed.horizontal,
     },
     widget_template = {
       {
         {
-          -- id = "icon_role",
-          -- widget = wibox.widget.imagebox,
-          id = "clienticon",
-          widget = awful.widget.clienticon,
+          {
+            id = "clienticon",
+            widget = awful.widget.clienticon,
+            resize = true,
+          },
+          margins = 1,
+          widget = wibox.container.margin,
         },
-        margins = {
-          left = 0,
-          right = 0,
-          top = 0,
-          bottom = 0,
-          -- left = 2,
-          -- right = 2,
-          -- top = 1,
-          -- bottom = 1,
-        },
-        widget = wibox.container.margin,
+        widget = wibox.container.background,
+        shape = require("lib.shape").rounded_rect(5),
       },
-      id = "background_role",
-      widget = wibox.container.background,
+      margins = -3,
+      widget = wibox.container.margin,
       create_callback = function(self, c, _idx, _clients)
         self:get_children_by_id("clienticon")[1].client = c
-        -- self:get_children_by_id("icon_role")[1].client = c
+
         local timed = require("vendor.rubato").timed({
           duration = 0.3,
           intro = 0.0,
           easing = require("vendor.rubato").easing.linear,
           subscribed = function(pos)
-            if c == client.focus then
-              return
-            end
-            self.bg = require("lib.color").interpolate(
+            self.children[1].bg = require("lib.color").interpolate(
               beautiful.bg_normal,
               beautiful.bg_focus,
               pos
@@ -111,17 +75,33 @@ function M.new(s)
           end,
           clamp_position = true,
         })
+
+        if c.active then
+          timed.target = 1
+        else
+          timed.target = 0
+        end
+
+        c:connect_signal("focus", function()
+          timed.target = 1
+        end)
+
+        c:connect_signal("unfocus", function()
+          timed.target = 0
+        end)
+
         self:connect_signal("mouse::enter", function()
           timed.target = 1
-          awesome.emit_signal("bling::task_preview::visibility", s, true, c)
+          preview:show(c)
         end)
+
         self:connect_signal("mouse::leave", function()
           timed.target = 0
-          awesome.emit_signal("bling::task_preview::visibility", s, false, c)
+          preview:hide()
         end)
       end,
     },
   })
 end
 
-return M
+return TaskList
